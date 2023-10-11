aa0388e3796d22f958463ae68b0fab1c087915c8
sahathat <61311677+sahathat@users.noreply.github.com>
2023-10-11 10:59:16 +0700

    Update index.js

--- vue/leafy-front/src/route/index.js
+++ vue/leafy-front/src/route/index.js
@@ -1,6 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../view/Home.vue'
-const history=createWebHistory(import.meta.env.VITEBASE_URL)
+const history=createWebHistory(import.meta.env.VITE_BASE_URL)
 
 const routes=[
     {   path:'/',
