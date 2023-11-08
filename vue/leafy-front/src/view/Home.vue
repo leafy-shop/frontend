b6cd641587b09b0d3a0b06d7b92550df7925bc31
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-11-08 15:57:47 +0700

    Update : Move component

--- vue/leafy-front/src/view/Home.vue
+++ vue/leafy-front/src/view/Home.vue
@@ -1,8 +1,10 @@
 <script setup>
-import BaseMenu from '../components/Home_page/BaseMenu.vue' 
+import BaseMenu from '../components/BaseMenu.vue' 
 import BaseInfomation from '../components/home_page/BaseInformation.vue'
 import BaseSolution from '../components/home_page/BaseSolution.vue'
 import BaseRandDesigner from '../components/home_page/BaseRandDesigner.vue';
+import BaseHistory from '../components/home_page/BaseHistory.vue';
+import BaseFooter from '../components/BaseFooter.vue';
 </script>
 
 <template>
@@ -10,6 +12,9 @@ import BaseRandDesigner from '../components/home_page/BaseRandDesigner.vue';
 <BaseInfomation/>
 <BaseSolution/>
 <BaseRandDesigner/>
+<BaseHistory/>
+
+<BaseFooter/>
 </template>
 
 <style scoped>
