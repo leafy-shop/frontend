8dab2695153f88166ee5ab7f00c88fc291b3fce8
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-11-09 16:27:50 +0700

    Update : Home : เพิ่ม Component ใหม่

--- vue/leafy-front/src/view/Home.vue
+++ vue/leafy-front/src/view/Home.vue
@@ -2,9 +2,11 @@
 import BaseMenu from '../components/BaseMenu.vue' 
 import BaseInfomation from '../components/home_page/BaseInformation.vue'
 import BaseSolution from '../components/home_page/BaseSolution.vue'
-import BaseRandDesigner from '../components/home_page/BaseRandDesigner.vue';
-import BaseHistory from '../components/home_page/BaseHistory.vue';
-import BaseFooter from '../components/BaseFooter.vue';
+import BaseRandDesigner from '../components/home_page/BaseRandDesigner.vue'
+import BaseHistory from '../components/home_page/BaseHistory.vue'
+import BaseLandScapeDesign from '../components/home_page/baselandscapedesign.vue'
+import BasePlantInspo from '../components/home_page/BasePlantInspo.vue'
+import BaseFooter from '../components/basefooter.vue'
 </script>
 
 <template>
@@ -13,7 +15,8 @@ import BaseFooter from '../components/BaseFooter.vue';
 <BaseSolution/>
 <BaseRandDesigner/>
 <BaseHistory/>
-
+<BaseLandScapeDesign/>
+<BasePlantInspo/>
 <BaseFooter/>
 </template>
 
