acaf1af11516fadef270afebf502fb5b00c45b40
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-11-12 16:24:18 +0700

    Update : index.js : add link path

--- vue/leafy-front/src/route/index.js
+++ vue/leafy-front/src/route/index.js
@@ -1,12 +1,20 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../view/Home.vue'
+import Shop from '../view/Shop.vue'
 const history=createWebHistory(import.meta.env.VITEBASE_URL)
 
 const routes=[
     {   path:'/',
         name:'Home',
-        component: Home
-    }
+        component: Home,
+    },
+    {   path:'/shop',
+        name:'Home',
+        component: Shop,
+    },
+    
+    
+    
 ]
 
 const router=createRouter({history,routes})
