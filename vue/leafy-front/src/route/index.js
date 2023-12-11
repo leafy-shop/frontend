ac80bfc83735ecf4c64243df2d54b82cb5c9f891
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-12-11 14:20:46 +0700

    Update index.js

--- vue/leafy-front/src/route/index.js
+++ vue/leafy-front/src/route/index.js
@@ -2,7 +2,9 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../view/Home.vue'
 import Shop from '../view/Shop.vue'
 import Login from '../view/Login.vue'
-const history=createWebHistory(import.meta.env.VITEBASE_URL)
+// const history=createWebHistory(import.meta.env.VITEBASE_URL)
+const history=createWebHistory('/pl4')
+
 
 const routes=[
     {   path:'/',
