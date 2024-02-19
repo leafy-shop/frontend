d98442af1d5ef1a0602254bdbd87fca69899aaf6
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-02-19 11:56:56 +0700

    Update BaseDescription.vue

--- vue/leafy-front/src/components/productDetail/BaseDescription.vue
+++ vue/leafy-front/src/components/productDetail/BaseDescription.vue
@@ -16,13 +16,13 @@ let props = defineProps({
         </h5>
         <!-- for show content long text -->
         <div class="description">
-            <p>
+            <!-- <p>
                 Polyscias are versatile, small, indoor, branching trees. They grow in a compact, upright manner, making them suitable even in smaller spaces. Their thick, woody trunks are their main appeal. Each plant is unique as the large, rounded leaves can develop anywhere along the length of the branches, exposing different degrees of woody stem on each plant. They are often called Dinner Plate Aralia due to their round, flat leaf shape. Polyscias translates from the Greek as "many" and "shade", a reference to their abundant foliage.
             </p>
             <br>
             <p>
                 Polyscias Scutellaria Fabian has large, scalloped, dark green, glossy leaves that curve outwards slightly. They are deeply veined and tinged purple underneath.
-            </p>
+            </p> -->
             <p>
                 <pre>{{ description }}</pre>
             </p>
