05d76d4e5b446c228c7a66eab4f86c428176c33c
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-03 16:42:41 +0700

    version 4.2
    Release 4 sprint 2
    ----------------------------------------
    BaseMenu (U):
    - แก้ไขปุ่มให้นำไปยังหน้า Account setting (Profile)

--- vue/leafy-front/src/components/BaseMenu.vue
+++ vue/leafy-front/src/components/BaseMenu.vue
@@ -97,7 +97,7 @@ const goShop=()=>{
 }
 const goCartList=()=>myRouter.push({name:'CartList'})
 const goProfile=(id=0)=>myRouter.push({name:'Profile',params:{id:validation.encrypt(id.toString())}})
-
+const goSetting=()=>myRouter.push({name:'profile_AS'})
 const signOut = async () => {
     let isOut=await fetch.signOut()
     if(isOut){
@@ -217,7 +217,7 @@ onMounted(()=>{
                         <button @click="goProfile(keyPass.id)">
                             My Profile
                         </button>
-                        <button>
+                        <button @click="goSetting">
                             Settings
                         </button>
                     </div>
@@ -300,7 +300,7 @@ onMounted(()=>{
                             My profile
                         </button>
                        
-                        <button class="account_bt">
+                        <button @click="goSetting" class="account_bt">
                             Settings
                         </button>
                         <button class="signOut" @click="signOut">
