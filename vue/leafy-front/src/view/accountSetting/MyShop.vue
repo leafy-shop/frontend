a24efb0dec85f1170b44d55b9025039954a15531
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-15 21:43:56 +0700

    version 4.2

--- vue/leafy-front/src/view/accountSetting/MyShop.vue
+++ vue/leafy-front/src/view/accountSetting/MyShop.vue
@@ -1,7 +1,34 @@
 <script setup>
 </script>
 <template>
-    my shop
+<div class="wrapper_my_shop">
+    <div class="my_shop">
+
+    </div>
+</div>
 </template>
 <style scoped>
+*{
+    box-sizing: border-box;
+}
+.wrapper_my_shop{
+    display: flex;
+    width: 100%;
+    height: fit-content;
+    padding: 20px;
+    gap: 24px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    border: none;
+    border-radius: 8px;
+    background-color: #fff;
+}
+.my_shop{
+    display: flex;
+    width: 100%;
+    height: inherit;
+    flex-direction: column;
+    gap: 24px;
+}
 </style>
