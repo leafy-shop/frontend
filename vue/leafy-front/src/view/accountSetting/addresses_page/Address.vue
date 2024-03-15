e1eb28d4bdf694c65f08d25446dc91eb1ec06193
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-15 21:41:58 +0700

    version 4.2

--- vue/leafy-front/src/view/accountSetting/addresses_page/Address.vue
+++ vue/leafy-front/src/view/accountSetting/addresses_page/Address.vue
@@ -1,24 +1,76 @@
 <script setup>
 import {useRouter} from 'vue-router'
-import {ref} from 'vue'
+import {ref,onBeforeMount} from 'vue'
+import fetch from '../../../JS/api';
+import cookie from '../../../JS/cookie';
+import validation from '../../../JS/validation'
 // link
 const myRouter =useRouter()
-const goAdd=()=>myRouter.push({name:'Address_AS_add'})
+const goAdd=()=>myRouter.push({name:'Address_AS_add',params:{method:'new-address'}})
+const goUpdate=(id)=>myRouter.push({name:'Address_AS_add',params:{method:'edit-address',id:validation.encrypt(id)}})
+
 
 // for display confirm delete
 const isDelete=ref(false)
-const confirmAddress=(input=false)=>{
-    if(input) isDelete.value=false;
+// common attribute
+const userName=ref('')
+const addressList=ref([])
+const addressId=ref('')
+
+const showConfirm=(id)=>{
+    isDelete.value=true
+    addressId.value=id
+}
+const confirmAddress=async(input=false)=>{
+    if(!input) isDelete.value=false;
     else{
         
         // delete api
-        console.log("delete function")
+        // console.log("delete function")
+        if(addressId.value.length!=0){
+            await deleteAddress();
+            // console.log(addressId.value)
+        }
+        else{
+            isDelete.value=false
+            console.log("close overlay")
 
+        }
+    }
+}
+
+const getAddress=async()=>{
+    let {username}=cookie.decrypt()
+    userName.value=username
+    let {status,msg,data}=await fetch.getAllAddress(username)
+    if(status){
+        addressList.value=data
+        console.log(data)
+    }
+}
+const deleteAddress =async()=>{
+    // console.log(addressId.value)
+    // let status =true
+    let {status,msg}= await fetch.deleteAddressById(userName.value,addressId.value)
+    if(status){
         // close overlay
         isDelete.value=false
         console.log("close overlay")
+        await getAddress()
+    }else {
+        //error 
+        isDelete.value=false
+        console.log("close overlay")
+        console.log(msg)
     }
+    
 }
+
+onBeforeMount(async()=>{
+    
+    // console.log(userName.value)
+    await getAddress()
+})
 </script>
 <template>
 <div class="wrapper_address">
@@ -36,28 +88,28 @@ const confirmAddress=(input=false)=>{
         </div>
         <div class="container_address">
             <div class="address_list">
-                <div class="address_item">
+                <div v-for="(address,index) of addressList" :key="index" class="address_item">
                         <!-- title -->
                         <div class="title">
                             <div class="info">
                                 <h5>
-                                    Apple Juice
+                                    {{address.addressname}}
                                 </h5>
                                 <p>
-                                    (+66) 991234567
+                                    {{address.phone}}
                                 </p>
                             </div>
                             
                             <div class="operation">
                                     <!-- edit -->
-                                <button>
+                                <button @click="goUpdate(address.addressId)">
                                     <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M7.16671 3.16664H3.00004C2.55801 3.16664 2.13409 3.34223 1.82153 3.65479C1.50897 3.96736 1.33337 4.39128 1.33337 4.83331V14C1.33337 14.442 1.50897 14.8659 1.82153 15.1785C2.13409 15.491 2.55801 15.6666 3.00004 15.6666H12.1667C12.6087 15.6666 13.0327 15.491 13.3452 15.1785C13.6578 14.8659 13.8334 14.442 13.8334 14V9.83331M12.655 1.98831C12.8088 1.82912 12.9927 1.70215 13.196 1.6148C13.3994 1.52746 13.6181 1.48148 13.8394 1.47956C14.0607 1.47763 14.2801 1.5198 14.485 1.6036C14.6898 1.6874 14.8759 1.81116 15.0324 1.96765C15.1889 2.12414 15.3126 2.31022 15.3964 2.51505C15.4802 2.71988 15.5224 2.93934 15.5205 3.16064C15.5185 3.38194 15.4726 3.60064 15.3852 3.80398C15.2979 4.00732 15.1709 4.19123 15.0117 4.34497L7.85671 11.5H5.50004V9.14331L12.655 1.98831Z" stroke="#9E9E9E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                     </svg>
 
                                 </button>
                                 <!-- bin -->
-                                <button @click="isDelete=true">
+                                <button @click="showConfirm(address.addressId)">
                                     <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M6.33337 8.16667V13.1667M9.66671 8.16667V13.1667M1.33337 4.83333H14.6667M13.8334 4.83333L13.1109 14.9517C13.0809 15.3722 12.8928 15.7657 12.5843 16.053C12.2758 16.3403 11.8699 16.5 11.4484 16.5H4.55171C4.13016 16.5 3.72426 16.3403 3.41578 16.053C3.10729 15.7657 2.91914 15.3722 2.88921 14.9517L2.16671 4.83333H13.8334ZM10.5 4.83333V2.33333C10.5 2.11232 10.4122 1.90036 10.256 1.74408C10.0997 1.5878 9.88772 1.5 9.66671 1.5H6.33337C6.11236 1.5 5.9004 1.5878 5.74412 1.74408C5.58784 1.90036 5.50004 2.11232 5.50004 2.33333V4.83333H10.5Z" stroke="#9E9E9E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                     </svg>
@@ -66,7 +118,8 @@ const confirmAddress=(input=false)=>{
                         </div>
                         <!-- discription -->
                         <p>
-                            King Mongkut’s University of Technology Thonburi (KMUTT), 126 Pracha Uthit Rd, Bang Mot, Thung Khru, Bangkok 10140
+                            {{ `${address.address}  ${address.province}  ${address.distrinct}
+                              ${address.subDistrinct}  ${address.postalCode}`}}
                         </p>                 
                 </div>
             </div>
