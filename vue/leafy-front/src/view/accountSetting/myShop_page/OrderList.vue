2f6d90a62d9c1a4f29bfea1c16645d46bec151d1
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-17 14:41:59 +0700

    version 4.3
    เตรียมโครงสร้างเอาไว้

---
+++ vue/leafy-front/src/view/accountSetting/myShop_page/OrderList.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+<template>
+<div class="wrapper_orders">
+    <div class="orders">
+
+    </div>
+</div>   
+</template>
+<style scoped>
+*{
+    box-sizing: border-box;
+}
+.wrapper_orders{
+    display: flex;
+    width: 100%;
+    height: fit-content;
+    padding: 20px;
+    gap: 24px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    border: none;
+    border-radius: 8px;
+    background-color: #fff;
+}
+.orders{
+    display: flex;
+    width: 100%;
+    height: inherit;
+    flex-direction: column;
+    gap: 24px;
+}
+</style>
