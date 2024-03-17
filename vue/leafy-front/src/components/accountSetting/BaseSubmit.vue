dab79817a5a98e683dfebc84b235e79038c9fef2
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-17 19:30:56 +0700

    version 4.3
    ทำเผื่อไว้เป็น Component

---
+++ vue/leafy-front/src/components/accountSetting/BaseSubmit.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props=defineProps({
+    cancel:{
+        type:Function,
+        required:true
+    },
+    submit:{
+        type:Function,
+        required:true
+    }
+})
+</script>
+<template>
+    <!-- submit -->
+    <div class="submit">
+        <button @click="goBanks()">
+            Cancel
+        </button>
+        <button @click="bankSubmit()">
+            Save
+        </button>
+    </div>
+</template>
+<style scoped>
+</style>
