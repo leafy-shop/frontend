0ef3925c73152e240d868840a19e599751e2138f
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-17 14:40:35 +0700

    version 4.3
    ทำให้สามารถลบข้อมูลของธนาคารได้

--- vue/leafy-front/src/view/accountSetting/bank_page/Bank.vue
+++ vue/leafy-front/src/view/accountSetting/bank_page/Bank.vue
@@ -107,14 +107,14 @@ onBeforeMount(async()=>{
                             
                             <div class="operation">
                                     <!-- edit -->
-                                <button @click="goUpdate()">
+                                <button @click="goUpdate(bank.paymentId)">
                                     <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M7.16671 3.16664H3.00004C2.55801 3.16664 2.13409 3.34223 1.82153 3.65479C1.50897 3.96736 1.33337 4.39128 1.33337 4.83331V14C1.33337 14.442 1.50897 14.8659 1.82153 15.1785C2.13409 15.491 2.55801 15.6666 3.00004 15.6666H12.1667C12.6087 15.6666 13.0327 15.491 13.3452 15.1785C13.6578 14.8659 13.8334 14.442 13.8334 14V9.83331M12.655 1.98831C12.8088 1.82912 12.9927 1.70215 13.196 1.6148C13.3994 1.52746 13.6181 1.48148 13.8394 1.47956C14.0607 1.47763 14.2801 1.5198 14.485 1.6036C14.6898 1.6874 14.8759 1.81116 15.0324 1.96765C15.1889 2.12414 15.3126 2.31022 15.3964 2.51505C15.4802 2.71988 15.5224 2.93934 15.5205 3.16064C15.5185 3.38194 15.4726 3.60064 15.3852 3.80398C15.2979 4.00732 15.1709 4.19123 15.0117 4.34497L7.85671 11.5H5.50004V9.14331L12.655 1.98831Z" stroke="#9E9E9E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                     </svg>
 
                                 </button>
                                 <!-- bin -->
-                                <button @click="showConfirm()">
+                                <button @click="showConfirm(bank.paymentId)">
                                     <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M6.33337 8.16667V13.1667M9.66671 8.16667V13.1667M1.33337 4.83333H14.6667M13.8334 4.83333L13.1109 14.9517C13.0809 15.3722 12.8928 15.7657 12.5843 16.053C12.2758 16.3403 11.8699 16.5 11.4484 16.5H4.55171C4.13016 16.5 3.72426 16.3403 3.41578 16.053C3.10729 15.7657 2.91914 15.3722 2.88921 14.9517L2.16671 4.83333H13.8334ZM10.5 4.83333V2.33333C10.5 2.11232 10.4122 1.90036 10.256 1.74408C10.0997 1.5878 9.88772 1.5 9.66671 1.5H6.33337C6.11236 1.5 5.9004 1.5878 5.74412 1.74408C5.58784 1.90036 5.50004 2.11232 5.50004 2.33333V4.83333H10.5Z" stroke="#9E9E9E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                     </svg>
@@ -131,16 +131,16 @@ onBeforeMount(async()=>{
         </div>
     </div>   
 
-    <div v-show="isDelete" @click="confirmAddress" class="wrapper_confirm_delete">
+    <div v-show="isDelete"  class="wrapper_confirm_delete">
         <div class="confirm_delete">
             <h5>
                 Do you want to delete the current bank account?
             </h5>
             <div>
-                <button @click="confirmAddress(true)">
+                <button @click="confirmBank()">
                     Cancel
                 </button>
-                <button @click="confirmAddress">
+                <button @click="confirmBank(true)">
                     Delete
                 </button>    
             </div>
