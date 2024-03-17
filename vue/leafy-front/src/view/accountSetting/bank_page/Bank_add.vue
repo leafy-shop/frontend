dc99af0f43f47a8ffdef6a60f1b683862572fe0d
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-17 14:41:20 +0700

    version 4.3
    ทำให้สามารถใช้งานโดยการส่งข้อมูลไปยัง Back-end ได้

--- vue/leafy-front/src/view/accountSetting/bank_page/Bank_add.vue
+++ vue/leafy-front/src/view/accountSetting/bank_page/Bank_add.vue
@@ -6,9 +6,9 @@ import validation from '../../../JS/validation'
 import cookie from '../../../JS/cookie'
 // link
 const myRouter=useRouter()
-const goAddress=()=>myRouter.push({name:'Bank_AS'})
+const goBanks=()=>myRouter.push({name:'Bank_AS'})
 //common attribute
-const bankOrigin={}
+const bankOrigin=ref({})
 const {params} =useRoute()
 const userName=ref('')
 const paymentId=ref('')
@@ -39,10 +39,24 @@ const bankNameM=ref('')
 
 const isPayment=computed(()=>{
     let returnData ={status:false,data:{}}
-    if(paymentName.value!=bankOrigin.value){
-        // ...
+    console.log(bankOrigin.value)
+    //ชื่อ
+    if(paymentName.value!=bankOrigin.value.bankname){
+        returnData.status=true
+        returnData.data['bankname']=paymentName.value
     }
-    return 
+    // เลขธนาคาร
+    if(accountNumber.value!=bankOrigin.value.bankAccount){
+        returnData.status=true
+        returnData.data['bankAccount']=accountNumber.value
+    }
+    // ชื่อย่อธนาคาร
+    if(bankName.value!=bankOrigin.value.bankCode){
+        returnData.status=true
+        returnData.data['bankCode']=bankName.value
+    }
+    
+    return returnData
 })
 
 // get only use edit mode
@@ -50,9 +64,11 @@ const getPaymentById=async()=>{
     let {status,msg,data}=await fetch.getPaymentById(userName.value,paymentId.value)
     if(status){
         bankOrigin.value=data
-        console.log(bankOrigin.value)
+        // console.log(bankOrigin.value)
         // assign value
-        
+        paymentName.value=data.bankname
+        accountNumber.value=data.bankAccount
+        bankName.value=data.bankCode        
     }
 }
 
@@ -63,28 +79,59 @@ const bankSubmit=async()=>{
         submitStatus=false
         paymentNameS.value=true
         paymentNameM.value="Please input your payment' name"
+        console.log('submit')
+
     }
     if(accountNumber.value.length==0){
         submitStatus=false
         accountNumberS.value=true
         accountNumberM.value="Please input your account number"
+        console.log('submit')
+
     }
     if(bankName.value.length==0){
         submitStatus=false
         bankNameS.value=true
         bankNameM.value="Please input your bank name"
+        console.log('submit')
+
     }
+    // console.log('submit')
 
     if(submitStatus){
-        let data={
-            bankName:paymentName.value,
-            bankCode:bankName.value,
-            bankAccount:accountNumber.value
+        // console.log(bank)
+        // select mode
+        if(!isEditMode.value){
+        // console.log("not edit mode")
+
+            // let data={
+            //     bankName:paymentName.value,
+            //     bankCode:bankName.value,
+            //     bankAccount:accountNumber.value
+            // }
+            console.log(isPayment.value.data)
+            // console.log(data)
+            let {status,msg}=await fetch.addPayment(isPayment.value.data)
+            if(status){
+                console.log("add payment success")
+                goBanks()
+            }    
+        }else //edit mode
+        if(isEditMode.value&&paymentId.value.length!=0){
+            // console.log('edit mode')
+            // check only data that change  if not can not edit
+            if(isPayment.value.status){
+                console.log(isPayment.value.data)
+                let {status,msg} =await fetch.updatePaymentById(userName.value,paymentId.value,isPayment.value.data)
+                if(status){
+                    console.log('update success')
+                    goBanks()
+                }else{
+                    // error from server
+                }
+            }
         }
-        let {status,msg}=await fetch.addPayment(data)
-        if(status){
-            console.log("add payment success")
-        }
+        
     }
 }
 
@@ -94,9 +141,10 @@ onBeforeMount(async()=>{
     userName.value=username 
     // check params
     if(params.id!=undefined&&params.id.length!=0){
-        validation.decrypt(params.id)
+        paymentId.value=validation.decrypt(params.id)
+        // console.log(validation.decrypt(params.id))
         isEditMode.value=true
-        console.log(params.id)
+        // console.log(params.id)
         await getPaymentById()
     }
 })
@@ -114,6 +162,17 @@ onBeforeMount(async()=>{
                     Name
                 </h5>
                 <input v-model="paymentName" class="input" type="text">
+                <!-- worning -->
+                <div v-show="paymentNameS" class="wrapper_errorMsg">
+                    <div >
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 8.99951C1.5 4.86026 4.86 1.49951 9 1.49951C13.1475 1.49951 16.5 4.86026 16.5 8.99951C16.5 13.1403 13.1475 16.4995 9 16.4995C4.86 16.4995 1.5 13.1403 1.5 8.99951ZM8.34 6.15701C8.34 5.79776 8.64 5.49701 9 5.49701C9.36 5.49701 9.6525 5.79776 9.6525 6.15701V9.47201C9.6525 9.83276 9.36 10.1245 9 10.1245C8.64 10.1245 8.34 9.83276 8.34 9.47201V6.15701ZM9.0075 12.5103C8.64 12.5103 8.3475 12.2103 8.3475 11.8503C8.3475 11.4903 8.64 11.1978 9 11.1978C9.3675 11.1978 9.66 11.4903 9.66 11.8503C9.66 12.2103 9.3675 12.5103 9.0075 12.5103Z" fill="#F75555"/>
+                        </svg>
+                        <p>
+                            {{paymentNameM}}
+                        </p>
+                    </div>
+                </div>
             </div>
                 
             <!-- Account_number -->
@@ -121,7 +180,18 @@ onBeforeMount(async()=>{
                 <h5>
                     Account number
                 </h5>
-                <input v-model="accountNumber" class="input" type="text">
+                <input v-model="accountNumber" class="input" type="text" maxlength="12">
+                <!-- worning -->
+                <div v-show="accountNumberS" class="wrapper_errorMsg">
+                    <div >
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 8.99951C1.5 4.86026 4.86 1.49951 9 1.49951C13.1475 1.49951 16.5 4.86026 16.5 8.99951C16.5 13.1403 13.1475 16.4995 9 16.4995C4.86 16.4995 1.5 13.1403 1.5 8.99951ZM8.34 6.15701C8.34 5.79776 8.64 5.49701 9 5.49701C9.36 5.49701 9.6525 5.79776 9.6525 6.15701V9.47201C9.6525 9.83276 9.36 10.1245 9 10.1245C8.64 10.1245 8.34 9.83276 8.34 9.47201V6.15701ZM9.0075 12.5103C8.64 12.5103 8.3475 12.2103 8.3475 11.8503C8.3475 11.4903 8.64 11.1978 9 11.1978C9.3675 11.1978 9.66 11.4903 9.66 11.8503C9.66 12.2103 9.3675 12.5103 9.0075 12.5103Z" fill="#F75555"/>
+                        </svg>
+                        <p>
+                            {{accountNumberM}}
+                        </p>
+                    </div>
+                </div>
             </div>
 
             <!-- Bank name -->
@@ -134,11 +204,22 @@ onBeforeMount(async()=>{
                     <option value="" selected hidden>Select your bank account</option>
                     <option v-for="(bank,index) of  bankList" :key="index" :value="bank.value">{{ bank.name }} </option>
                 </select>
+                <!-- worning -->
+                <div v-show="bankNameS" class="wrapper_errorMsg">
+                    <div >
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 8.99951C1.5 4.86026 4.86 1.49951 9 1.49951C13.1475 1.49951 16.5 4.86026 16.5 8.99951C16.5 13.1403 13.1475 16.4995 9 16.4995C4.86 16.4995 1.5 13.1403 1.5 8.99951ZM8.34 6.15701C8.34 5.79776 8.64 5.49701 9 5.49701C9.36 5.49701 9.6525 5.79776 9.6525 6.15701V9.47201C9.6525 9.83276 9.36 10.1245 9 10.1245C8.64 10.1245 8.34 9.83276 8.34 9.47201V6.15701ZM9.0075 12.5103C8.64 12.5103 8.3475 12.2103 8.3475 11.8503C8.3475 11.4903 8.64 11.1978 9 11.1978C9.3675 11.1978 9.66 11.4903 9.66 11.8503C9.66 12.2103 9.3675 12.5103 9.0075 12.5103Z" fill="#F75555"/>
+                        </svg>
+                        <p>
+                            {{bankNameM}}
+                        </p>
+                    </div>
+                </div>
             </div>
             
             <!-- submit -->
             <div class="submit">
-                <button @click="goAddress()">
+                <button @click="goBanks()">
                     Cancel
                 </button>
                 <button @click="bankSubmit()">
@@ -201,7 +282,7 @@ onBeforeMount(async()=>{
 .input_field{
     display: flex;
     width: 100%;
-    height: 60px;
+    height: fit-content;
     gap: 4px;
     flex-direction: column;
 }
@@ -249,5 +330,36 @@ onBeforeMount(async()=>{
     background-color: #26AC34;
     color: #fff;
 }
-
+.wrapper_errorMsg{
+    display: flex;
+    width: 100%;
+    height: fit-content;
+    flex-direction: column;
+    padding: min(0.556dvw,8px) min(0.833dvw,12px);
+    font-size: min(0.833dvw,12px);
+    border-radius: min(0.278dvw,4px);
+    background-color: rgba(245, 72, 74, 0.08);
+    gap: min(0.556dvw,8px)
+}
+.wrapper_errorMsg div{
+    display: flex;
+    width: inherit;
+    height: inherit;
+    gap: min(0.278dvw,4px);
+}
+.wrapper_errorMsg div svg{
+    width: min(1.042dvw,15px);
+    height: min(1.042dvw,15px);
+}
+.wrapper_errorMsg div p{
+    width: 100%;
+    font-size:min(0.833dvw,12px);
+    font-style: normal;
+    font-weight: 400;
+    line-height: 136%; /* 16.32px */
+    letter-spacing: 0.2px;
+    color: #F75555;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
 </style>
