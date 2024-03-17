b8338bfa1f8d0c5fbae08e42afa76840af3eb89e
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-17 19:32:01 +0700

    version 4.3
    ทำไฟล์เอาไว้เก็บตัวแปรไว้เลย ของธนาคาร

---
+++ vue/leafy-front/src/JS/enum/bankAccount.js
@@ -0,0 +1,13 @@
+const BANKCODE = Object.freeze([
+    {name:"ธนาคารกรุงเทพ จำกัด ( BBL )",value:"BBL"},
+    {name:"ธนาคารกสิกรไทย ( KBANK )",value:"KBANK"},
+    {name:"ธนาคารไทยพาณิชย์ ( SCB )",value:"SCB"},
+    {name:"ธนาคารกรุงศรีอยุธยา ( BAY )",value:"BAY"},
+    {name:"ธนาคารออมสิน ( GSB )",value:"GSB"},
+    {name:"ธนาคารกรุงไทย ( KTB )",value:"KTB"},
+    {name:"ธนาคารทหารไทย ( TMB )",value:"TMB"},
+    {name:"ธนาคารทหารไทยธนชาติ ( TTB )",value:"TTB"},
+    {name:"ธนาคารซิตี้แบงค์ ( CITI )",value:"CITI"},
+])
+
+export default BANKCODE 
