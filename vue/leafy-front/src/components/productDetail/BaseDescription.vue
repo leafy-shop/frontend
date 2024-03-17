485e7582f450dd6e0162b189b0c8bc15ab5f0b03
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-17 14:36:26 +0700

    version 4.2
    แก้ให้อยู่ในบรรทัดเดียวกันจากข้อความที่ยาวมากๆ ให้รวมกันสวยงามมากขึ้น

--- vue/leafy-front/src/components/productDetail/BaseDescription.vue
+++ vue/leafy-front/src/components/productDetail/BaseDescription.vue
@@ -1,12 +1,19 @@
 <script setup>
-
-let props = defineProps({
+import { computed, onBeforeMount, onMounted, onUpdated } from 'vue';
+const props = defineProps({
     description: {
         type: String,
         required: true,
         default: ""
     }
 })
+const text =`Polyscias are versatile, small, indoor, branching trees. They grow in a compact, upright manner, making them suitable even in smaller spaces. Their thick, woody trunks are their main appeal. Each plant is unique as the large, rounded leaves can develop anywhere along the length of the branches, exposing different degrees of woody stem on each plant. They are often called Dinner Plate Aralia due to their round, flat leaf shape. Polyscias translates from the Greek as "many"" and ""shade"", a reference to their abundant foliage.\n\nPolyscias Scutellaria Fabian has large, scalloped, dark green, glossy leaves that curve outwards slightly. They are deeply veined and tinged purple underneath."`
+// onMounted(()=>{
+    // haiku.replace(/\n/g, "<br />")
+//    console.log(typeof(text))
+//    console.log(props.description.replace(/\n/g, "<br />"))
+// document.getElementById('content').innerHTML=``+props.description
+// })
 
 </script>
 <template>
@@ -23,9 +30,9 @@ let props = defineProps({
             <p>
                 Polyscias Scutellaria Fabian has large, scalloped, dark green, glossy leaves that curve outwards slightly. They are deeply veined and tinged purple underneath.
             </p> -->
-            <p>
-                <pre>{{ description }}</pre>
-            </p>
+            <div id="content"  >
+                {{text}}
+            </div>
         </div>
     </div>
 </template>
@@ -45,8 +52,11 @@ let props = defineProps({
     font-size: min(1.389dvw, 20px);
     line-height: 160%;
 }
-.description p{
+.description div{
     font-size: min(0.972dvw, 14px);
     letter-spacing: min(0.014dvw, 0.2px);
+    word-break: normal;
+    white-space: pre-line
 }
+
 </style>
