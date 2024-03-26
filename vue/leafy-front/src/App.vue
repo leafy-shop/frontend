23d366643bb8b0649aa1ca349eb451b2968cb79e
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-03-26 23:31:55 +0700

    version 4.4
    none

--- vue/leafy-front/src/App.vue
+++ vue/leafy-front/src/App.vue
@@ -6,7 +6,12 @@
   <!-- use this key to reload page -->
   <router-view :key="$route.fullPath"></router-view>
 </template>
-<!-- <style>
-@custom-media --narrow-window (max-width: 30em);
-</style> -->
+<style>
+/* @custom-media --narrow-window (max-width: 30em); */
+.menu {
+    position: sticky;
+    top: 0;
+    z-index: 999;
+}
+</style>
 
