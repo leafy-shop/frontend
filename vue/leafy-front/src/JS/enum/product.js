c80f759e3244b449db5ee8ee908b7d21d4c953f7
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-04-01 20:44:33 +0700

    version 4.5
    เพิ่ม Enum สำหรับเก็บค่าคงที่

--- vue/leafy-front/src/JS/enum/product.js
+++ vue/leafy-front/src/JS/enum/product.js
@@ -1,9 +1,22 @@
-const sortTypeArr = Object.freeze([
-    { name: "Popular", value: { name: "popular", type: 'desc' } },
-    { name: "New Arrival", value: { name: "new_arrival", type: 'desc' } },
-    { name: "Top Sales", value: { name: "sales", type: 'desc' } },
-    { name: "Low - High", value: { name: "price", type: 'asc' } },
-    { name: "High - Low", value: { name: "price", type: 'desc' } },
-])
+const productEnum={
 
-export default sortTypeArr
+
+    sortTypeArr : Object.freeze([
+        { name: "Popular", value: { name: "popular", type: 'desc' } },
+        { name: "New Arrival", value: { name: "new_arrival", type: 'desc' } },
+        { name: "Top Sales", value: { name: "sales", type: 'desc' } },
+        { name: "Low - High", value: { name: "price", type: 'asc' } },
+        { name: "High - Low", value: { name: "price", type: 'desc' } },
+    ]),
+    itemType : Object.freeze([
+        {name:'Plant',value: 'plant'},
+        {name:'Flower',value: 'flower'}, 
+        {name:'Cactus',value: 'cactus'}, 
+        {name:'Seed',value: "seed"}, 
+        {name:'Equirement',value: "equirement"}, 
+        {name:'Material',value: "material"},
+    ])
+}
+// module.exports.sortTypeArr=sortTypeArr
+// module.exports.itemType =itemType
+export default productEnum
