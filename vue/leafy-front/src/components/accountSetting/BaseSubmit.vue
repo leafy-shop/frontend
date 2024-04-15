8f950b2a391461fd5b1e15678eb4c85bf7230766
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-04-15 21:54:05 +0700

    version 4.6
    ทำเป็น Component

--- vue/leafy-front/src/components/accountSetting/BaseSubmit.vue
+++ vue/leafy-front/src/components/accountSetting/BaseSubmit.vue
@@ -1,25 +1,66 @@
 <script setup>
 const props=defineProps({
-    cancel:{
-        type:Function,
-        required:true
+    name:{
+        type:String,
+        required:true,
+        default:''
     },
-    submit:{
-        type:Function,
-        required:true
+    disabled:{
+        type:Boolean,
+        default:false
     }
 })
 </script>
 <template>
     <!-- submit -->
     <div class="submit">
-        <button @click="goBanks()">
+        <button @click="$emit('goBanks')">
             Cancel
         </button>
-        <button @click="bankSubmit()">
+        <button @click="$emit('bankSubmit')" :disabled="props.disabled" :class="[props.disabled==true?'btn_disable':'']">
             Save
         </button>
     </div>
 </template>
 <style scoped>
+*{
+    box-sizing: border-box;
+}
+.btn_disable{
+    cursor: not-allowed !important;
+    background-color: #BDBDBD !important;
+    border: none !important;
+}
+.submit {
+    display: flex;
+    width: 100%;
+    height: 60px;
+    padding: 12px 20px;
+    gap: 8px;
+    justify-content: end;
+    background-color: #FAFAFA;
+}
+
+.submit button {
+    width: 80px;
+    height: 36px;
+    border: 1px solid;
+    padding: 8px 12px;
+    border-radius: 4px;
+    box-shadow: 0px 1px 2px 0px #0000000D;
+    cursor: pointer;
+}
+
+.submit button:nth-child(1) {
+    border-color: #E0E0E0;
+    background-color: #fff;
+    color: #212121;
+}
+
+.submit button:nth-child(2) {
+    border-color: #26AC34;
+    background-color: #26AC34;
+    color: #fff;
+}
+
 </style>
