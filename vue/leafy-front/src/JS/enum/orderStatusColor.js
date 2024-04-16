da9c40ba6f5dd0fce509d322bee534350ed635c8
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-04-17 02:54:35 +0700

    version 4.6
    เพิ่มสถานะ สำหรับจ่ายเงิน

--- vue/leafy-front/src/JS/enum/orderStatusColor.js
+++ vue/leafy-front/src/JS/enum/orderStatusColor.js
@@ -1,5 +1,6 @@
 let ORDERSTATUSCOLOR = Object.freeze([
     {font:'#FACC15',bg:'#FFD30014'}, //pending
+    {font:'#FACC15',bg:'#FFD30014'}, //pending
     {font:'#235DFF',bg:'#235DFF14'}, //in progress
     {font:'#12D18E',bg:'#17CE9214'}, //completed
     {font:'#F75555',bg:'#F5484A14'}, //canceled
