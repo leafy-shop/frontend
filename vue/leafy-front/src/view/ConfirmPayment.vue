c1376f1b5d00eda56e59496016a0efaede7ac7bd
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-04-17 16:16:01 +0700

    version 4.7
    สร้างรอไว้

---
+++ vue/leafy-front/src/view/ConfirmPayment.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+<template>
+    <div class="wrapper_confirm_payment">
+        <div class="confirm_payment">
+
+        </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
