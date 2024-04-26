83a7af19c96b069a2e3d77c195e73ecee9e8355e
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-04-26 21:51:20 +0700

    version 4.7
    Responsive design

--- vue/leafy-front/src/components/BaseEmptyList.vue
+++ vue/leafy-front/src/components/BaseEmptyList.vue
@@ -32,23 +32,23 @@ const showEmpty=computed(()=>props.showEmpty)
     box-sizing: border-box;
 }
 .wrapper_empty{
-    display: flex;
-    width: 100%;
-    height: min(13.889dvw,200px);
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color:#fff;
-    border: none;
-    border-radius: min(0.278dvw,4px);
+  display: flex;
+  width: 100%;
+  height: min(13.889dvw,200px);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color:#fff;
+  border: none;
+  border-radius: min(0.278dvw,4px);
 }
 .wrapper_empty > h4{
-    width: fit-content;
-    height: fit-content;
-    font-size: min(0.972dvw,14px);
-    line-height: 144%;
-    font-weight: 500;
-    color:#212121;
+  width: fit-content;
+  height: fit-content;
+  font-size: min(0.972dvw,14px);
+  line-height: 144%;
+  font-weight: 500;
+  color:#212121;
 }
 /* HTML: <div class="loader"></div> */
 .loader {
@@ -73,5 +73,38 @@ const showEmpty=computed(()=>props.showEmpty)
   100% {
     inset: 0 0 0 min(2.431dvw,35px);
   }
+}
+
+/* mobile */
+@media (width<=432px){
+  .wrapper_empty{
+      height: 200px;
+      border-radius: 0px;
+  }
+  .wrapper_empty > h4{
+    font-size: 14px;
+  }
+  .loader {
+    width: 64px;
+    height: 28px;
+  }
+  .loader:before {
+    content: "";
+    position: absolute;
+    border-radius: 50px;
+    box-shadow: 0 0 0 3px inset #26AC34;
+    /* animation: l3 0.75s infinite alternate; */
+  }
+  @keyframes l3 {
+  0% {
+    inset: 0 35px 0 0;
+  }
+  50% {
+    inset: 0 0 0 0;
+  }
+  100% {
+    inset: 0 0 0 35px;
+  }
+}
 }
 </style>
