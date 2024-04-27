8144aab20c5ab7d2224569f8dcb0c2b71bc9c155
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-04-27 21:19:05 +0700

    version 4.7
    Responsive design

---
+++ vue/leafy-front/src/JS/enum/orderDisplayWord.js
@@ -0,0 +1,20 @@
+let ORDERSTATUSDISPLAY = Object.freeze([
+    "To Pay",
+    "To Ship",
+    "To Receive",
+    "Completed",
+    "Cancelled"
+])
+//     {
+//     // supplier
+//     required: "To Pay",
+//     pending: "To Ship",
+//     in_progress: "To Receive",
+//     // driver
+//     // INTRANSIT: "in transit",
+//     // DELIVERED: "delivered",
+//     // customer
+//     completed: "Completed",
+//     canceled: "Canceled"
+// }
+export default ORDERSTATUSDISPLAY
