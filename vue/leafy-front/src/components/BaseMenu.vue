5341f1de521a13c6420dcd96ae4479c44b9ec0b7
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-04-27 21:17:45 +0700

    version 4.7
    Responsive design

--- vue/leafy-front/src/components/BaseMenu.vue
+++ vue/leafy-front/src/components/BaseMenu.vue
@@ -208,12 +208,12 @@ onUpdated(async()=>{
                     <button @click="goGalleryList" class="link" name="gallery">
                         Gallery
                     </button>
-                    <button class="link " name="about">
+                    <!-- <button class="link " name="about">
                         About
                     </button>
                     <button class="link contact">
                         Contact
-                    </button>
+                    </button> -->
                 </div>
             </div>
             <div class="element_service">
@@ -348,8 +348,7 @@ onUpdated(async()=>{
                         </svg>
                         <h3> Gallery</h3>
                     </button>
-                    <button @click="validation.clickingTest('link about')" name="about">
-                        <!-- <img src="../assets/icon/about.png" alt="about_icon"> -->
+                    <!-- <button @click="validation.clickingTest('link about')" name="about">
                         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 15.7463V11.7993" stroke="#212121" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" />
@@ -363,14 +362,13 @@ onUpdated(async()=>{
                         <h3> About</h3>
                     </button>
                     <button @click="validation.clickingTest('link contact')" name="contact">
-                        <!-- <img src="../assets/icon/contact.png" alt="contact_icon"> -->
                         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M3.58579 3.58579C3.21071 3.96086 3 4.46957 3 5V6C3 14.284 9.716 21 18 21H19C19.5304 21 20.0391 20.7893 20.4142 20.4142C20.7893 20.0391 21 19.5304 21 19V15.721C21.0001 15.511 20.934 15.3064 20.8112 15.136C20.6885 14.9657 20.5152 14.8383 20.316 14.772L15.823 13.274C15.5947 13.1981 15.3466 13.2071 15.1244 13.2993C14.9021 13.3915 14.7205 13.5607 14.613 13.776L13.483 16.033C11.0345 14.9267 9.07332 12.9655 7.967 10.517L10.224 9.387C10.4393 9.27945 10.6085 9.0979 10.7007 8.87564C10.7929 8.65339 10.8019 8.40534 10.726 8.177L9.228 3.684C9.16171 3.48496 9.03449 3.3118 8.86436 3.18905C8.69422 3.0663 8.48979 3.00016 8.28 3H5C4.46957 3 3.96086 3.21071 3.58579 3.58579Z"
                                 stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                         <h3> Contact</h3>
-                    </button>
+                    </button> -->
                 </div>
 
                 <div class="service_container_2">
